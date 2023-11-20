b0579ec7533f352ebe0b443afd2cac80dd02ea7b
pai-puella <cat.dyshkant@yandex.ru>
2023-11-20 09:31:08 -0800

    Edited class Bicycle

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,7 +1,12 @@
 public class Bicycle {
 
-    public String modelName;
-    public int wheelsCount;
+    private String modelName;
+    private int wheelsCount;
+
+    public Bicycle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
